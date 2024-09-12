84511cd7b0b97bc5aaaa3237819c99f653d4d9a1
deveshwadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2024-09-12 13:25:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,24 +2,24 @@ const container = document.querySelector('.country-container')
 fetch('data.json')
     .then((res) => res.json())
     .then((data) => data.forEach(country => {
-        let countryCard = document.createElement('div')
+        let countryCard = document.createElement('a')
+        countryCard.href = `/country.html?country=${country.name}`
         countryCard.classList.add('country-card')
-        countryCard.innerHTML = `<div class="country-card">
+        countryCard.innerHTML = `
         <div class="img-container">
             <img src="${country.flags.svg}" style="object-fit: cover;"alt="${(country.name).toLowerCase()}-flag" height=100% width=100%>
         </div>
-            <div class="country-info">
+            <div class="country-info" title="${country.name}">
                 <h2 class="country-name">${country.name}</h2>
                 <div class="more-info">
                     <h5 class="population">Population: <span class="value">${country.population}</span></h5>
                     <h5 class="region">Region: <span class="value">${country.region}</span></h5>
                     <h5 class="capital">Capital: <span class="value">${country.capital}</span></h5>
                 </div>
-            </div>
         </div>`
         container.appendChild(countryCard)
     })
-    )
+)
 
 const ibutton = document.querySelector('#mode i')
 const mode = document.querySelector('#mode')
